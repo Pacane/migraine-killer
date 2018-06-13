52846292cbc166d9918d68d9b9525403a0b7bbe7
Joel Trottier-Hebert <iamquebecois@gmail.com>
2018-06-12 20:25:28 -0400

    Icon + notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:migraine_killer/pages.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) => MaterialApp(
         title: 'Migraine Killer',
@@ -12,4 +20,31 @@ class MyApp extends StatelessWidget {
         ),
         home: MenuPage(),
       );
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scheduleReminderNotification();
+  }
+
+  Future _scheduleReminderNotification() async {
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+      'mk_001',
+      'reminder_night',
+      'quiz reminder night',
+      icon: 'think_36',
+      largeIcon: 'think_48',
+    );
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await FlutterLocalNotificationsPlugin().showDailyAtTime(
+      0,
+      'Rappel quotidien',
+      "N'oubliez-pas de remplir vos questionnaires aujourd'hui!",
+      Time(22),
+      platformChannelSpecifics,
+    );
+  }
 }
